246c31cb85f53973a9a5f35d2acb446a8d3b7cb1
ramonpdx03 <63172652+ramonpdx03@users.noreply.github.com>
2024-07-30 23:54:23 -0300

    Delete server directory

--- server/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from'mysql';
-import 'dotenv/config';
-
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
