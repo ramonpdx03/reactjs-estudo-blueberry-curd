72d807e096c0fcc8cdd1d6a0037fb29b4b8aa2ae
ramonpdx03 <63172652+ramonpdx03@users.noreply.github.com>
2024-07-30 23:54:13 -0300

    Delete app directory

--- app/src/components/Checkbox.jsx
+++
@@ -1,8 +0,0 @@
-export default function Checkbox({ type = "checkbox", id, name, children, disabled = false, className = '', ...props }) {
-  return (
-    <label htmlFor={id} className={`checkbox ${className}`}>
-      <input type={type} id={id} name={name} disabled={disabled} {...props} />
-      <span className={`text-sm ${disabled ? 'text-slate-400' : ''}`}>{children}</span>
-    </label>
-  );
-}
--- app/src/pages.js
+++
@@ -1,9 +0,0 @@
-import Login from "./pages/Login/Login";
-import CriarConta from "./pages/Login/CriarConta";
-import EsqueciSenha from "./pages/Login/EsqueciSenha";
-
-export {
-  Login,
-  CriarConta,
-  EsqueciSenha,
-};
